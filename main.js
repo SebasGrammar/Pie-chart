901671347e7838555e5b24d097ea592456096534
Sebastian <sebasgrammar@hotmail.com>
2020-01-11 02:05:53 -0500

    circles are finally being displayed

--- main.js
+++ main.js
@@ -3,19 +3,30 @@ const pieChart = document.querySelector(".pie-chart")
 
 let data = [105, 20, 32, 70, 43]
 
+let colors = ["red", "blue", "orange", "gray", "brown"]
+
 let percentages = data.map(number => number * 31.42 / 100)
+
+// let attributes = {
+//     r: "5",
+//     cx: "10",
+//     cy: "10",
+//     fill: "transparent",
+//     stroke: "tomato",
+//     "stroke-width": "10",
+    
+
+
+  
+// }
 
 let attributes = {
     r: "5",
     cx: "10",
     cy: "10",
     fill: "transparent",
-    stroke: "tomato",
     "stroke-width": "10",
     
-
-
-  
 }
 
 /*
@@ -28,8 +39,11 @@ function setThis(percentage) {
     return `${percentage} 31.42`
 }
 
+function setColor(index) {
+    return colors[index]
+}
 
-percentages.forEach(percentage => {
+percentages.forEach((percentage, index) => {
     //const slice =  document.createElement("circle")
     const slice = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
@@ -38,6 +52,7 @@ percentages.forEach(percentage => {
     // slice.style.setProperty("transform-origin", "50%")
     // slice.style.setProperty("transform", "rotate(-72deg)")
     slice.setAttribute("stroke-dasharray", setThis(percentage))
+    slice.setAttribute("stroke", setColor(index))
     /*
     `<circle class="${percentage}" r="5" cx="10" cy="10" fill="transparent" stroke="tomato" stroke-width="10"
     stroke-dasharray="${percentage} 31.42" />`
