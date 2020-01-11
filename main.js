42809cc5d69ed1240faa5c5ba6a850ebba440ce0
Sebastian <sebasgrammar@hotmail.com>
2020-01-11 01:51:09 -0500

    came across an issue I'm trying to solve

---
+++ main.js
@@ -0,0 +1,52 @@
+const pieChart = document.querySelector(".pie-chart")
+
+
+let data = [105, 20, 32, 70, 43]
+
+let percentages = data.map(number => number * 31.42 / 100)
+
+let attributes = {
+    r: "5",
+    cx: "10",
+    cy: "10",
+    fill: "transparent",
+    stroke: "tomato",
+    "stroke-width": "10",
+    
+
+
+  
+}
+
+/*
+"stroke-dasharray": function(percentage) {
+    return `${percentage} 31.42`
+}
+*/
+
+function setThis(percentage) {
+    return `${percentage} 31.42`
+}
+
+
+percentages.forEach(percentage => {
+    const slice =  document.createElement("circle")
+
+    Object.keys(attributes).forEach(attribute => slice.setAttribute(attribute, attributes[attribute]))
+
+    // slice.style.setProperty("transform-origin", "50%")
+    // slice.style.setProperty("transform", "rotate(-72deg)")
+    slice.setAttribute("stroke-dasharray", setThis(percentage))
+    /*
+    `<circle class="${percentage}" r="5" cx="10" cy="10" fill="transparent" stroke="tomato" stroke-width="10"
+    stroke-dasharray="${percentage} 31.42" />`
+    */
+    slice.classList.add(`circle-${Math.round(percentage)}`)
+    console.log(slice)
+    pieChart.appendChild(slice)
+
+})
+
+console.log(percentages)
+
+console.log(pieChart)
